606a1eccd7178030aeddfd9b9be405a58b532ac5
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-01-22 09:47:55 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// не статический класс описывающий точку 
+    /// </summary>
+    internal class Point
+    {
+        /// <summary>
+        /// внутренние поля описывающие координаты точек 
+        /// </summary>
+        private double x, y;
+        /// <summary>
+        ///Внешнее поле для подсчета количества созданных экземпляров 
+        /// </summary>
+        public static int count;
+        /// <summary>
+        ///  статический конструктор Point(), который инициализирует статическую переменную count нулевым значением;
+        /// </summary>
+        static Point()
+        {
+            count = 0;
+        }
+        /// <summary>
+        ///    нестатический конструктор Point(double, double) с двумя параметрами, которые инициализируют одноименные внутренние поля класса;
+        /// </summary>
+        /// <param name="x">описывающий поле х</param>
+        /// <param name="y">описывающий поле у</param>
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            //   count++;
+            Point.count++;
+        }
+        /// <summary>
+        /// нестатический конструктор Point() без параметров, который инициализирует внутренние поля класса значением 1.0;
+        /// </summary>
+        public Point()
+        {
+            this.x = 1.0;
+            this.y = 1.0;
+            count++;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double getX()
+        {
+            return x;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double getY()
+        {
+            return y;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        public void setX(double x)
+        {
+            this.x = x;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="y"></param>
+        public void setY(double y)
+        {
+            this.y = y;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Координаты точки:{getX():f1}\t{getY():f1}";
+        }
+        static void LengthPoints(Point[] points)
+        {
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Point point = new Point();
+            double x = 0, y = 0;
+            ////point.setX(x);
+            ////point.setY(y);
+            ////Console.WriteLine(point.ToString());
+            //Point[] points= new Point[4];
+            Random rnd = new Random();
+            //for (int i = 0; i < points.Length; i++) 
+            //{
+            //      x = rnd.NextDouble() * 10 - 5;
+            //    y = rnd.NextDouble() * 10 - 5;
+            //    points[i] = new Point(x, y);
+            //}
+            //foreach (Point p in points)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
