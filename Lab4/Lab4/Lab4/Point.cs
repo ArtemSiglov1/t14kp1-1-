fa3aa49ad32734caa0086b8f8137dbd7d8d9426b
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-13 15:50:25 +0200

    Add files via upload

---
+++ Lab4/Lab4/Lab4/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string NameR {  get; set; }
+        public Point (int x, int y,string namer)
+        {
+            X = x;
+            Y = y;
+            NameR = namer;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"x={X},y={Y}, Название точки:{NameR}");
+        }
+        public double S(int x1,int y1,int x2,int y2)
+        {
+            return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+        }
+    }
+}
---
+++ Lab4/Lab4/Lab4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Ввести размерност коллекции:");
+            //int n=Convert.ToInt32(Console.ReadLine());
+            //List<Point>list = new List<Point>(n);
+            //for(int i = 0; i < list.Capacity; i++)
+            //{
+            //    Console.Write("Введите x=");
+            //    int x= Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Введите y=");
+            //    int y= Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Введите название точки:");
+            //    string namer = Console.ReadLine();
+            //    list.Add(new Point(x,y,namer));
+            //}
+            //Console.Write("Ввести индекс точки старта:");
+            //int start = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести индекс точки конца:");
+            //int end = Convert.ToInt32(Console.ReadLine());
+            //if (list[start].S(list[start].X, list[start].Y, list[start + 1].X, list[start + 1].Y) < list[start].S(list[start].X, list[start].Y, list[start - 1].X, list[start - 1].Y))
+            //{
+
+            //    //три точки
+            //    Console.WriteLine($"{list[start].NameR}:{list[start + 1].NameR}:{list[end].NameR}");
+            //}
+            //else
+            //{
+            //    //Три точки
+            //    Console.WriteLine($"{list[start].NameR}:{list[end - 1].NameR}:{list[end].NameR}");
+            //}
+
+            var g = new Graph();
+
+            //добавление вершин
+            g.AddVertex("A");
+            g.AddVertex("B");
+            g.AddVertex("C");
+            g.AddVertex("D");
+            g.AddVertex("E");
+            g.AddVertex("F");
+            g.AddVertex("G");
+
+            //добавление ребер
+            g.AddEdge("A", "B", 22);
+            g.AddEdge("A", "C", 33);
+            g.AddEdge("A", "D", 61);
+            g.AddEdge("B", "C", 47);
+            g.AddEdge("B", "E", 93);
+            g.AddEdge("C", "D", 11);
+            g.AddEdge("C", "E", 79);
+            g.AddEdge("C", "F", 63);
+            g.AddEdge("D", "F", 41);
+            g.AddEdge("E", "F", 17);
+            g.AddEdge("E", "G", 58);
+            g.AddEdge("F", "G", 84);
+
+            var dijkstra = new Dijkstra(g);
+            var path = dijkstra.FindShortestPath("A", "G");
+            Console.WriteLine(path);
+            Console.ReadKey();
+        }
+    }
+}
