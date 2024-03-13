48ef3f696386a7d235780fb7d5193bef45a49ec4
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-13 15:51:31 +0200

    Add files via upload

---
+++ LAB7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Sklad.Sklad1 sklad11 = new Sklad.Sklad1("Motors", 10, 200);
+                Sklad.Sklad1 sklad1 = new Sklad.Sklad1("Car", 5, 20000);
+                Sklad.Sklad1 sklad12 = new Sklad.Sklad1("Wheel", 20, 50);
+                Sklad.Sklad1 sklad = Sklad.Sklad1.Init();
+                List<Sklad.Sklad1> sklads = new List<Sklad.Sklad1> { sklad1, sklad11, sklad12, sklad };
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine($"1-Info\n2-all item price\nend");
+                    int i = int.Parse(Console.ReadLine());
+                    switch (i)
+                    {
+                        case 1:
+                            foreach (var item in sklads)
+                            {
+                                Console.WriteLine(item.ToString());
+                            }
+                            break;
+                        case 2:
+                            foreach (var item in sklads)
+                            {
+                                Console.WriteLine(item.AllPrice());
+                            }
+                            break;
+                        case 3: flag = false; break;
+
+                        default: Console.WriteLine("not correct data"); break;
+                    }
+                }
+            }
+            catch { Main(); }
+            
+
+        }
+    }
+}
---
+++ LAB7/Sklad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB7
+{
+    internal class Sklad
+    {
+       
+       public struct Sklad1
+        {
+            public string Name {  get; set; }
+            public int Count {  get; set; }
+            public int Price {  get; set; }
+            public Sklad1(string name,int count,int price) 
+            {
+             Name = name;
+                Count = count;
+                Price = price;
+            } 
+            public int AllPrice()
+            {
+                Console.Write(Name+":");
+                return Price*Count;
+            }
+            public override string ToString()
+            {
+                return $"Name:{Name}\nCount:{Count}\nPrice:{Price}";
+            }
+            public static Sklad1 Init()
+            {
+                try
+                { 
+                Console.Write("Name:");
+                string name = Console.ReadLine();
+                Console.Write("Count:");
+                int count = int.Parse(Console.ReadLine());
+                Console.Write("Price:");
+                int price = int.Parse(Console.ReadLine());
+                return new Sklad1(name, count, price);
+                }
+                catch { return Init(); }
+            }
+        }
+    }
+}
