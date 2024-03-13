72418b4cfd27cbab44e9fae4151d10fda3ca451f
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-13 15:50:47 +0200

    Add files via upload

---
+++ LAB5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadacha zadacha = Zadacha.Init();
+            Console.WriteLine(zadacha.Time());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB5/Zadacha.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5
+{
+    internal class Zadacha
+    {
+        public int Speed1 {  get; set; }
+        public int Speed2 { get; set;}
+        public int Rast {  get; set;}
+        public Zadacha() { }
+        public Zadacha(int speed1, int speed2,int rast) 
+        {
+            Speed1 = speed1;
+            Speed2 = speed2;
+            Rast = rast;
+        }
+        public double Time()
+        {
+            int speedRaz = Speed2 - Speed1;
+            double time = Rast / speedRaz;
+            return time;
+        }
+        public static Zadacha Init() 
+        {
+            try
+            {
+                Console.Write("Speed1:");
+                int speed1 = int.Parse(Console.ReadLine());
+                Console.Write("Speed2:");
+                int speed2 = int.Parse(Console.ReadLine());
+                Console.Write("Rast:");
+                int rast = int.Parse(Console.ReadLine());
+                return new Zadacha(speed1, speed2, rast);
+            } 
+            catch { return Init(); }
+        }
+    }
+}
