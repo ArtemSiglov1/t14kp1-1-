41ef85a3001145ab287458eb63ad91d59b9ffb69
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-27 23:29:12 +0200

    Add files via upload

---
+++ LAB12/LAB12/PC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB12
+{
+    internal class PC
+    {
+        public string Name {  get; set; }
+        public PC() { }
+        public PC(string name)
+        {
+            Name = name;
+        }
+        public static Exception Iron()
+        {
+            return new Exception("отказ «железа»");
+        }
+        public static Exception NoCorrectMove()
+        {
+            return new Exception("неправильные действия оператора");
+        }
+        public static Exception MisteakOS()
+        {
+            return new Exception("сбой в операционной системе");
+        }
+        public static Exception Peregrev()
+        {
+            return new Exception("перегрев");
+        }
+        public static Exception AllExceptoin()
+        {
+            Random random = new Random();
+            switch (random.Next(1,5))
+            {
+                case 1:return Iron();
+                case 2:return NoCorrectMove();
+                case 3:return MisteakOS();
+                case 4:return Peregrev();
+                default:return new Exception("Работа Коректная");
+            }
+        }
+    }
+}
